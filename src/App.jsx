3206f7cef26f8e6618fbdfbbb3618f00c9b3094b
Hossein <hosseinpro@gmail.com>
2024-06-22 13:50:43 +0430

    add list of the input inside of the ul

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,42 @@ import { useState } from 'react'
 import './main'
 
 export default function App() {
+  const [newItem, setNewItem] = useState('');
+
+  const [todos, setTodos] = useState([])
+
+
+  function handleSubmit (e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, {id:crypto.randomUUID(), title:newItem, completed: false},
+      ]
+    })
+  }
   
   return ( 
     <>
-  <form className='new-item-form'>
+  <form className='new-item-form' onSubmit={handleSubmit}>
     <div className="form-row">
       <label htmlFor='item' >New Item</label>
-      <input  type="text" id='item' />
+      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id='item' />
     </div>
     <button className="btn">Add</button>
   </form>
   <h1 className='header'>Todo List</h1>
   <ul className="list">
-    <li>
+    {todos.map(todo=> {
+      return  <li>
       <label >
-        <input type="checkbox" />
-        item1
+        <input type="checkbox" checked= {todo.completed} />
+        {todo.title}
       </label>
       <button className='btn btn-danger'>Delete</button>
     </li>
+    })}
+
   </ul>
   </>
   )
